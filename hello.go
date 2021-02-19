b28e2b9ee356d3a7cff7c5e59332473becd12695
Justin M <zhengyi13@gmail.com>
2021-02-18 21:15:40 -0800

    spelling error

--- hello.go
+++ hello.go
@@ -11,5 +11,5 @@ func main() {
 	var name = flag.String("name", "world", "to whom do you wish to say hello?")
 	flag.Parse()
 	fmt.Printf("Hello %s!\n", *name)
-	glog.Info("Prepage to repel boarders")
+	glog.Info("Prepare to repel boarders")
 }
